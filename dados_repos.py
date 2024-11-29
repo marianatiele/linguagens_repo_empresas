92850c9c00628e2d0b2f7979e986e9787d248c8c
Maria Natiele <marianatiele20@gmail.com>
2024-11-29 15:26:03 -0300

    Adicionando arquivos

---
+++ dados_repos.py
@@ -0,0 +1,86 @@
+import requests
+import pandas as pd
+
+class DadosRepositorios:
+
+    def __init__(self, owner):
+        self.owner = owner
+        self.api_base_url = 'https://api.github.com'
+        self.access_token = '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'
+        self.headers = {'Authorization' : 'Bearer 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',
+           'X-GitHub-Api-Version': '2022-11-28'}
+
+    
+    def list_repositorios(self):
+        repos_list = []
+        for page_num in range(1,20):
+            try:
+                    url = f'{self.api_base_url}/users/{self.owner}/repos?page={page_num}'
+                    response = requests.get(url, headers=self.headers)
+                    repos_list.append(response.json())
+
+            except:
+                repos_list.append(None)
+
+        return repos_list
+
+
+    def nomes_repos(self, repos_list):
+            
+        repos_name = []
+        for page in repos_list:
+            for repo  in page:
+                try:
+                    repos_name.append(repo['name'])
+                except:
+                    pass
+        return repos_name
+       
+    
+
+    def nomes_linguagens(self, repos_list):
+        repos_linguage = []
+        for page in repos_list:
+            for repo in page:
+                try:
+                    repos_linguage.append(repo['language'])
+                except:
+                    pass
+        
+        return repos_linguage     
+
+    def cria_df_linguagem(self):
+        repositorios = self.list_repositorios()
+        names = self.nomes_repos(repositorios)
+        linguagens = self.nomes_linguagens(repositorios)
+
+        dados = pd.DataFrame()
+        dados['repository_names'] = names
+        dados['linguagens'] = linguagens
+
+        return dados
+
+    
+    
+amz_repos = DadosRepositorios('amzn')
+ling_amz_usadas = amz_repos.cria_df_linguagem()
+        # print(ling_amz_usadas)
+
+netflix_repos = DadosRepositorios('netflix')
+ling_netflix_usadas = netflix_repos.cria_df_linguagem()
+        # print(ling_netflix_usadas)
+
+spotify_repos = DadosRepositorios('spotify')
+ling_spotify_usadas = spotify_repos.cria_df_linguagem()
+        # print(ling_spotify_usadas)
+
+        # Salvando dados
+ling_amz_usadas.to_csv('dados/ling_rep_amz.csv')
+
+ling_netflix_usadas.to_csv('dados/ling_rep_netiflix.csv')
+
+ling_spotify_usadas.to_csv('dados/ling_rep_spotify.csv')
+
+
+
+    
---
+++ manipula_dados.py
@@ -0,0 +1,63 @@
+import requests
+import base64
+from dados_repos import DadosRepositorios
+
+class ManipulaRepositorios:
+
+    def __init__(self, username):
+        self.username = username
+        self.api_base_url = 'https://api.github.com'
+        self.access_token = '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'
+        self.headers = {
+            'Authorization' : 'Bearer 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',
+           'X-GitHub-Api-Version': '2022-11-28'
+           
+        }
+    
+
+
+    def cria_repos(self, name_repo):
+
+        data = {
+            'name':name_repo,
+            'description':'Dados dos repositórios de algumas empresas',
+            'private':False
+
+        }
+
+        response = requests.post(f"{self.api_base_url}/user/repos", json=data, headers=self.headers)
+        print('STATUS CODE repositório: ', response.status_code)
+
+    
+
+    def add_arquivo(self, nome_repo, nome_arquivo, caminho_arquivo):
+
+        # Codificando o arquivo
+        with open(caminho_arquivo, "rb") as file:
+            file_content = file.read()
+        encoded_content = base64.b64encode(file_content)
+
+        # Fazendo o upload do arquivo
+        url = f"{self.api_base_url}/repos/{self.username}/{nome_repo}/contents/{nome_arquivo}"
+
+        data = {
+            'message':'Add um novo arquivo',
+            'content': encoded_content.decode('utf-8')
+        }
+
+        response = requests.put(url, json=data, headers=self.headers)
+        print(f'status_code upload do arquivo: {response.status_code}')
+
+
+# Enviando os arquivos 
+novo_repo = ManipulaRepositorios('marianatiele')
+
+nome_repo = 'linguagens_repo_empresas'
+
+novo_repo.cria_repos(nome_repo)
+
+novo_repo.add_arquivo(nome_repo, 'ling_rep_amz.csv', 'dados/ling_rep_amz.csv')
+
+novo_repo.add_arquivo(nome_repo, 'ling_rep_netiflix.csv', 'dados/ling_rep_netiflix.csv')
+
+novo_repo.add_arquivo(nome_repo, 'ling_rep_spotify.csv', 'dados/ling_rep_spotify.csv')
